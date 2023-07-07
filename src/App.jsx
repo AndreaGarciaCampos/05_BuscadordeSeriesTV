816d6926154826c246658b1d5ad4670ce62afabe
AndreaGarciaCampos <200053@mail.unacar.mx>
2023-07-07 11:21:04 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+import Poster from './Components/Poster'
+import './App.css'
+import SearchBar from './Components/SearchBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  const [pelis, setPelis] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.tvmaze.com/shows')
+      .then((response) => response.json())
+      .then((results) => {
+        console.log(results)
+        setPelis(results)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [])
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredData = pelis.filter((item) => {
+    return item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  return (
+    <div>
+      <header>
+        <div className='navbar navbar-dark bg-dark shadow-sm'>
+          <div className='container'>
+            <a href='#' className='navbar-brand d-flex align-items-center'>
+              <strong>My Movie App</strong>
+            </a>
+            <SearchBar handleSearchChange={handleSearch} />
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className='album py-5 bg-light'>
+          <div className='container'>
+            <div className='row row-cols-1 row-cols-md-4 g-4 align-items-stretch'>
+              {filteredData.map((peli) => (
+                <div className='col' key={peli.id}>
+                  <div className='card custom-card'>
+                    <div className='d-flex align-items-center'>
+                      <img
+                        src={peli.image.original}
+                        alt={peli.name}
+                        className='card-img-top poster-image'
+                      />
+                    </div>
+                    <div className='card-body'>
+                      <p className='card-text'>{peli.name}</p>
+                      <div className='d-flex justify-content-between align-items-center'>
+                        <small className='text-muted'>{peli.genres}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className='text-muted py-5'>
+        <div className='container'>
+          <p className='float-end mb-1' />
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Poster.jsx
@@ -0,0 +1,22 @@
+const Poster = ({ id, type, name, url }) => {
+  return (
+    <div className='col-md-4'>
+      <div className='card mb-4 shadow-sm'>
+        <img src={url} alt={name} className='card-img-top' />
+        <div className='card-body'>
+          <h2 className='card-title'>{name}</h2>
+          <h5 className='card-subtitle mb-2 text-muted'>Género: {type}</h5>
+          <p className='card-text'>This content is a little bit longer.</p>
+          <div className='d-flex justify-content-between align-items-center'>
+            <div className='btn-group'>
+              <button type='button' className='btn btn-sm btn-outline-secondary'>View</button>
+            </div>
+            <small className='text-muted'>9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Poster
